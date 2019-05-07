65dd82f32434be6eba21ce309ea51550112e0940
AndreyAh <ahramenko@ngs.ru>
2019-05-07 23:04:20 +0700

    Add ArrowToTop

--- source/js/common.js
+++ source/js/common.js
@@ -22,3 +22,20 @@
 //   });
 // });
 // Конец
+
+//Перемотка наверх
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > $(this).height()) {
+    $('.top').addClass('active');
+  } else {
+    $('.top').removeClass('active');
+  }
+});
+
+// действие кнопки вверх
+
+$('.top').click(function(){
+  $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
+//
