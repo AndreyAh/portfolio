e21ac99222db2d9fef6d4d3068ff54009826705d
AndreyAh <ahramenko@ngs.ru>
2019-05-09 11:44:59 +0700

    Added Yandex-Metrica

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,6 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
